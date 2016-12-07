665bda5ac89237ac44666838f8c9823842e0be81
Brimborion <charles.guerin.rouard@gmail.com>
2016-12-07 22:33:47 +0100

    TG-70 #in-progress * Front entity + service * Logger integration => Todo: production configuration, log with console.log from Winston

--- front/src/service/library.service.ts
+++ front/src/service/library.service.ts
@@ -2,16 +2,16 @@ import { Injectable } from '@angular/core';
 import {Http, Headers} from '@angular/http';
 import {Library} from '../entity/library';
 import {environment} from '../environments/environment';
-
-import 'rxjs/add/operator/toPromise';
 import {backend} from '../config/backend';
+import 'rxjs/add/operator/toPromise';
+import {HttpLoggerService} from './http-logger.service';
 
 @Injectable()
 export class LibraryService {
   private libraryUrl;
   private headers;
 
-  constructor(private http: Http) {
+  constructor(private http: Http, private httpLogger: HttpLoggerService) {
     this.libraryUrl = environment.api_url + backend.api.library;
     this.headers = new Headers({'Content-Type': 'application/json'});
   }
@@ -19,6 +19,14 @@ export class LibraryService {
   findLibraries(): Promise<Library[]> {
     return this.http.get(this.libraryUrl)
       .toPromise()
-      .then( response => response.json().data as Library[]);
+      .then(response => response.json().data as Library[])
+      .catch(error => this.httpLogger.error(error));
+  }
+
+  findLibrary(id: number): Promise<Library> {
+    return this.http.get(`${this.libraryUrl}/${id}`)
+      .toPromise()
+      .then(response => response.json().data as Library)
+      .catch(error => this.httpLogger.error(error));
   }
 }
