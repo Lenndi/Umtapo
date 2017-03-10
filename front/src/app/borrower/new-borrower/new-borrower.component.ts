209771ef305372c45d9d7f99c8db08bf19e4df35
Brimborion <charles.guerin.rouard@gmail.com>
2017-03-10 12:22:12 +0100

    in progress

--- front/src/app/borrower/new-borrower/new-borrower.component.ts
+++ front/src/app/borrower/new-borrower/new-borrower.component.ts
@@ -1,5 +1,6 @@
 import {Component} from '@angular/core';
 import {NewBorrowerDataService} from '../../../service/data-binding/new-borrower-data.service';
+import {NewBorrower} from './new-borrower.interface';
 
 @Component({
   selector: 'app-new-borrower',
@@ -9,5 +10,11 @@ import {NewBorrowerDataService} from '../../../service/data-binding/new-borrower
 })
 
 export class NewBorrowerComponent {
+  newBorrowerChild: NewBorrower;
+
   constructor(public dataService: NewBorrowerDataService) {}
+
+  saveData() {
+    this.newBorrowerChild.saveData();
+  }
 }
