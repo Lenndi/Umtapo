9ee5420a87a86123b8568bac4ba48e4785104e5c
Brimborion <charles.guerin.rouard@gmail.com>
2017-03-14 13:48:34 +0100

    TG-152 #ready-for-test * Update dependencies

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -15,7 +15,6 @@ import {SetupComponent} from './setup/setup.component';
 import {NewBorrowerComponent} from './borrower/new-borrower/new-borrower.component';
 import {BorrowerService} from '../service/borrower.service';
 import {SetupDataService} from '../service/data-binding/setup-data.service';
-import {MaterialModule} from '@angular/material';
 import 'hammerjs';
 import {ItemRegistrationComponent} from './cataloging/item-registration/item-registration.component';
 import {ItemSearchComponent} from './cataloging/item-registration/item-search/item-search.component';
